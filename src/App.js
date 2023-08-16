611da4399335dbe8ccd9fa3c2ff918158ef50773
Juan Diego Guerra <jdxelric@gmail.com>
2023-08-16 09:29:32 -0500

    Main Page Structure

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
-import Calculator from './components/calculator';
-import Quote from './components/Quote';
+import Home from './routes/home';
+import Calculator from './routes/calculator';
+import Quote from './routes/quote';
+import Layout from './components/Layout';
 
 function App() {
   return (
-    <>
-      <Calculator />
-      <Quote />
-    </>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="calculator" element={<Calculator />} />
+        <Route path="quote" element={<Quote />} />
+      </Route>
+    </Routes>
   );
 }
 
