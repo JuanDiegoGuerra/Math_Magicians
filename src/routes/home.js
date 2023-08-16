6f903ad4ccce86ca3870565c663211f30535dd96
Juan Diego Guerra <jdxelric@gmail.com>
2023-08-16 09:29:41 -0500

    Routes

---
+++ src/routes/home.js
@@ -0,0 +1,20 @@
+const Home = () => (
+  <div className="container home-container">
+    <h2 className="section-title">Welcome to my page!</h2>
+    <p className="section-description">
+      Are you ready to explore the fascinating world of mathematics? Look no
+      further! Math Magicians is your go-to platform for all things
+      math-related. Whether you need to perform complex calculations or seek
+      inspiration through insightful quotes, we have got you covered. Calculator
+      Feature: Our powerful calculator feature allows you to crunch numbers with
+      ease. Perform basic arithmetic operations, solve equations, and even
+      tackle advanced mathematical problems effortlessly. With Math Magicians,
+      you will never struggle with calculations again. Random Quote Feature: Need
+      a dose of inspiration? Our Random Quote feature connects you with a vast
+      collection of motivational quotes from various thinkers, mathematicians,
+      and visionaries. Get inspired, gain new perspectives, and fuel your
+      mathematical journey with these thought-provoking quotes.
+    </p>
+  </div>
+);
+export default Home;
---
+++ src/routes/quote.js
@@ -0,0 +1,8 @@
+import Quote from '../components/Quote';
+
+const QuoteSection = () => (
+  <div className="container quote-container">
+    <Quote />
+  </div>
+);
+export default QuoteSection;
